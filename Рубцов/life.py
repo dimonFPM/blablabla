f78d65e058b61fb2676a70b66f9a55f53a01ebe2
dimon2170 <dimons1s@yandex.ru>
2021-10-16 20:10:18 +0300

    16.10.2021 20:09 перевёл отрисовку сетки в проценты

--- Рубцов/life.py
+++ Рубцов/life.py
@@ -7,18 +7,40 @@ width_win, height_win = map(int, (root.winfo_screenwidth() * 0.5,
 root.geometry(f"{width_win}x{width_win}+0+0")
 root.resizable(False, False)
 
-size = 70  # int(input("введите размерность сетки: "))  # размерность сетки для поля
-shag = (width_win - 50) / size
+size = 50  # int(input("введите размерность сетки: "))  # размерность сетки для поля
+shag = (width_win - width_win * 0.07) / size
+
+
+# def paint_canvas(root, size):  # отрисовка поля
+#     canvas_for_field = tk.Canvas(root, width=width_win - 49, height=width_win - 49, bg="white")
+#     canvas_for_field.pack(side=tk.TOP, padx=10, pady=10)
+#     canvas_for_field.create_rectangle(2, 2, width_win - 48, width_win - 48, outline='black')
+#     for i in range(size - 1):  # от 0 до size
+#         canvas_for_field.create_line(2 + shag * (i + 1), 2, 2 + shag * (i + 1), width_win - 48)
+#     for i in range(size - 1):
+#         canvas_for_field.create_line(2, 2 + shag * (i + 1), width_win - 48, 2 + shag * (i + 1))
+#     return canvas_for_field
 
 
 def paint_canvas(root, size):  # отрисовка поля
-    canvas_for_field = tk.Canvas(root, width=width_win - 49, height=width_win - 49, bg="white")
-    canvas_for_field.pack(side=tk.TOP)
-    canvas_for_field.create_rectangle(2, 2, width_win - 48, width_win - 48, outline='black')
+    canvas_for_field = tk.Canvas(root, width=width_win - width_win * 0.07, height=width_win - width_win * 0.07,
+                                 bg="white")
+    canvas_for_field.pack(side=tk.TOP, padx=width_win * 0.015, pady=width_win * 0.015)
+    canvas_for_field.create_rectangle(0 + width_win * 0.003,
+                                      0 + width_win * 0.003,
+                                      width_win - width_win * 0.069,
+                                      width_win - width_win * 0.069,
+                                      outline='black')
     for i in range(size - 1):  # от 0 до size
-        canvas_for_field.create_line(2 + shag * (i + 1), 2, 2 + shag * (i + 1), width_win - 48)
+        canvas_for_field.create_line(0 + width_win * 0.003 + shag * (i + 1),
+                                     0 + width_win * 0.003,  # 2
+                                     0 + width_win * 0.003 + shag * (i + 1),
+                                     width_win - width_win * 0.069)
     for i in range(size - 1):
-        canvas_for_field.create_line(2, 2 + shag * (i + 1), width_win - 48, 2 + shag * (i + 1))
+        canvas_for_field.create_line(0 + width_win * 0.003,
+                                     0 + width_win * 0.003 + shag * (i + 1),
+                                     width_win - width_win * 0.069,
+                                     0 + width_win * 0.003 + shag * (i + 1))
     return canvas_for_field
 
 
@@ -42,20 +64,18 @@ print("\n")
 
 
 # заполнение тестового массива
-l = (0, 1, 4, 7, 3, 9, 46, 5, 43, 12, 7)
-l2 = (0, 1, 6, 3, 7, 5, 9, 34, 28, 6, 9)
-for i in range(len(l)):
-    list1[l[i]][l2[i]][0] = 1
-for i in range(len(list1)):
-    print(list1[i])
-#############################
+# l = (0, 1, 4, 7, 3, 9, 46, 5, 43, 12, 7)
+# l2 = (0, 1, 6, 3, 7, 5, 9, 34, 28, 6, 9)
+# for i in range(len(l)):
+#     list1[l[i]][l2[i]][0] = 1
+# for i in range(len(list1)):
+#     print(list1[i])
+# #############################
 
 canvas = paint_canvas(root, size)
-paint_circle(canvas, tuple(list1))
+# paint_circle(canvas, tuple(list1))
 
-#алгоритм
-
-
+# алгоритм
 
 
 #########
